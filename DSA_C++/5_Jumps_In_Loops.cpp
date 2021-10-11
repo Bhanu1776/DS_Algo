c1363ce3125963bce8546d348ae958b8cbc8d267
Bhanu <sunkabhanu281202@gmail.com>
2021-10-11 14:41:57 +0530

    Break and Continue statement

---
+++ DSA_C++/5_Jumps_In_Loops.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+//* We can control flow of loops on metting specified condition by break and continue statement ..
+
+// int main(){
+
+//     int pocketMoney = 300;
+
+//     for (int date=1 ; date<=30 ; date++){
+
+//         if (date%2==0){
+//             continue;           // continue == is used to skip to the next ittration of the loop 
+//         }
+        
+//         if (pocketMoney ==0){
+//             break;              // break == is used to terminate a loop 
+//         }
+//         cout<< "Go out Today!";
+//         pocketMoney= pocketMoney - 300;
+//     }
+
+//     return 0;
+// }
+
+//* Remove the numbers which are divisible by 3 in loop from 1 to 100 
+
+// int main (){
+
+//     int n= 100;
+//     for (int i=1; i<=n; i++){
+        
+//         if(i%3==0){
+//             continue;
+//         }
+//         cout<<i<<endl;
+//     }
+// }
