2a86465c4a78f2d7064f3389b8bbf145c50b154d
Bhanu <sunkabhanu281202@gmail.com>
2021-10-11 14:28:17 +0530

    Basic Syntax of C++

---
+++ DSA_C++/!_Basics.cpp
@@ -0,0 +1,13 @@
+//! Basic Syntax of C++ Language !!
+
+#include <iostream>              //* Header file :- Which include such files to use input output functions. 
+using namespace std;             // std = standard output .. specifying namespace is mandatory 
+
+int main(){                      //* The execution of program starts from main function ..
+
+    cout<< "HELLO BUNNY";        //* cout is used to display output of the program 
+                                            //* This "<<" is known as insertion operator 
+    std:: cout << "Have a nice Day !!";     // Second way, Use this if we dont write 4th line ..
+
+    return 0;                    //* Termination .. Exit of main function 
+}
