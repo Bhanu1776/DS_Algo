30334c5b0b75c87e99bf0566d6a007ea2df13896
Bhanu <sunkabhanu281202@gmail.com>
2021-10-11 14:31:06 +0530

    If else wtih two examples

---
+++ DSA_C++/3_if_elseif_else.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//* FIND GREATER NUMBER 
+
+// int main(){
+
+//     int a, b, c;
+//     a= 1;
+//     b= 15;
+//     c= 10;
+
+//     if (a>b && a>c){
+//         cout<< "A is greater";
+//     }
+//     else if (b>a && b>c){
+//         cout << "B is greater";
+//     }
+//     else{
+//         cout << "C is greater";
+//     }
+//     return 0;
+// }
+
+//* FIND THE GIVEN NUMBER IS ODD OR EVEN !
+
+// int main(){
+    
+//     int a;
+//     cin>>a;
+
+//     if (a%2==0){
+//         cout<< "Even";
+//     }
+//     else{
+//         cout << "Odd";
+//     }
+//     return 0;
+// }
