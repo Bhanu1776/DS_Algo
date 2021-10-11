6063754ccdc77d11c52b27828837ad17afe4465e
Bhanu <sunkabhanu281202@gmail.com>
2021-10-11 14:28:39 +0530

    cin and cout

---
+++ DSA_C++/2_Input,Output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int amount1;
+    cin >> amount1;                                 //* cin is used to take input and This ">>" is know as extraction operator  
+
+    int amount2;
+    cin >> amount2;
+
+    int sum = amount1+amount2;
+    cout << sum << endl;                //* endl is known for line break and /n and endl acts same 
+}
