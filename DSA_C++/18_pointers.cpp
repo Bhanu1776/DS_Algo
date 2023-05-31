559cf8e5bec0b86ecd0fb1e01204cb320d6fd31a
Bhanu Sunka <sunkabhanu281202@gmail.com>
2023-05-31 14:02:42 +0530

    Update more about pointers in char arrays and symbol table

--- DSA_C++/18_pointers.cpp
+++ DSA_C++/18_pointers.cpp
@@ -34,20 +34,35 @@ int main(){
         cout<<*ptr<<endl<<endl;
         ptr++;
     }
+    
+    int z = 2;                      //* Another way of getting elements in an array
+    cout<< i[arr] <<endl;           // Output -> 30
 
     //* Pointers to pointers 
 
     int b =10;
-    int* bptr;
+    int* bptr;              //* Bad practice of initalising pointer without declaration .. bcoz it stores garbage value in pointers that can result in readonly system faluts
     bptr = &b;
 
-    int** ppb = &bptr;      // Double Pointers, Here one pointer variable is storing the address of another pointer variable ..
+    int** ppb = &bptr;      //* Double Pointers, Here one pointer variable is storing the address of another pointer variable ..
 
     cout<<*ppb<<endl;       // Derefrencing ..This will give the value of bptr.. i.e address of b variable
     cout<<**ppb<<endl<<endl;      //* Double Derefrencing ..
 
-
-    //* Call by reference 
+    
+    //* Pointers in char arrays
+    char ch = "Bhanu";
+    cout<<ch<<endl;        //* This will not print address of char array.. It will simply return Declared String(Bhanu) 
+                           // Implementation of cout for char-array is different than Arrays.
+    char *chptr = &ch[0];
+    cou<<chptr<<endl;      // This is how we can get the address of first element in a char arrays
+    
+    //* All the address with their variable mapping are stored in symbol table - only compiler have access to symbol table
+    // We cannot overwrite any address defined in symbol table
+    // To know more about symbol table refer this video ==> https://youtu.be/rlpw7oi-bpE?t=1800
+    
+    
+    //* Call by reference (pointers in Functions)
 
     int c = 10;
     int d = 20;
@@ -55,6 +70,7 @@ int main(){
     swap(&c,&d);            // Sending the address of c and d to swap function ..
     cout<<a<<" "<<b<<endl;
 
+    
 
     return 0;
 }
