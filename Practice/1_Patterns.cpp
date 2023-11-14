8ce881229b5afbcd9225663d74ed390fcb1aaddc
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-14 15:47:38 +0530

    patterns

---
+++ Practice/1_Patterns.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//* Patters 
+// 1) For the number of lines, write in outer loop
+// 2) For the inner loop, focus on the columns & connect them somehow to the rows.
+// 3) Print them '*' inside the inner for loop
+// 4) Observe symmetry (optional)
+
+int main(){
+
+  // for(int i=0; i<4; i++){
+  //   for(int j=0; j<4; j++){
+  //     cout<<"*";
+  //   }
+  //   cout<<endl;
+
+
+  
+  }
+
+  return 0;
+}
