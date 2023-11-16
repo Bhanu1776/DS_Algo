f150eb418f846ae10dd99e7abc786f754cc95b23
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-16 12:33:32 +0530

    Remove Duplicates in-place from sorted array 2 approaches

--- revision.cpp
+++ revision.cpp
@@ -1,54 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Second Largest Element in an array
+//? Remove Duplicates in-place from sorted array
 
 int main(){
 
-  int arr[5] = {1,5,2,4,3};
+  int arr[5] = {1,2,3,3,5};
 
   //* Brute force approach
-  // sort(arr, arr+5);
-  // if(arr[3]!=arr[4])  cout<<arr[3];          // Not exactly correct tho!
-  // else cout<<arr[2];
+
+  // // Creating a set
+  // set<int> mySet;
+
+  // for(int i = 0; i<5; i++){
+  //   mySet.insert(arr[i]);
+  // }
+
+  // //Using Iterator, Transferring all the set values into an OG array
+  // int index=0;
+  // for(auto& element: mySet){
+  //   arr[index++] = element;
+  // }
+
+  // //Printing the array values
+  // for(int i = 0; i<4; i++){
+  //   cout<<arr[i];
+  // }
+  
 
 //* -------------------------------------------------------
 
   //* Better approach
-  // int sLargest = INT_MIN;
-  // int largest = arr[0];
 
-  // // First we need to find the largest number
-  // for(int i =0; i<5; i++){
-  //   if(arr[i]>largest){
-  //     largest = arr[i];
-  //   }
-  // }
-
-  // // Now we will find the second largest number
-  // for(int i=0; i<5; i++){
-  //   if(sLargest<arr[i] && arr[i]!=largest){
-  //     sLargest=arr[i];
-  //   }
-  // }
-  // cout<<sLargest<<endl;
-
-//* -------------------------------------------------------
-
-  //* Optimal Approach
-
-  int largest = arr[0];
-  int sLargest = INT_MIN;
-
-  for(int i=0; i<5; i++){
-    if(arr[i]>largest){
-      sLargest = largest;
-      largest = arr[i];
-    }
-    else if (arr[i]<largest && arr[i]>sLargest){    // In between
-      sLargest = arr[i];
+  int i = 0;
+  for(int j =1; j<5; j++){
+    if(arr[i]!=arr[j]){
+      arr[i+1] = arr[j];
+      i++;
     }
   }
-  cout<<sLargest;
-
+  cout<<"Size of a final array"<<i+1<<endl;
+  
 }
