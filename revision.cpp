60960400accd6e73abaea0c0bd8f6edc832debd3
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-17 11:29:14 +0530

    Two sum problem 3 approaches

--- revision.cpp
+++ revision.cpp
@@ -1,32 +1,86 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Maximum consecutive ones
+//? TwoSum problem
+
+vector<int> Brute(vector<int>& nums, int target) {
+  vector<int> outputArray;
+
+  for(int i =0; i<nums.size(); i++){
+      for(int j=i+1; j<nums.size(); j++){
+          if(nums[i]+nums[j]==target){
+              outputArray.push_back(i);
+              outputArray.push_back(j);
+              break;
+          }
+      }
+  }
+  return outputArray;
+}
+
+// Using Map
+vector<int> Better(vector<int>& nums, int target) {
+    map<int,int> output;
+    int n = nums.size();
+
+    for(int i = 0; i<n; i++){
+        int num = nums[i];
+        int val = target - nums[i];
+
+        if(output.find(val) != output.end()){
+            return {output[val], i};
+        }
+        output[num] = i;
+    }
+    return {-1, -1};
+}
+
+// Using two pointer approach - different variant
+string Optimal(vector<int>& nums, int target) {
+    int n = nums.size();
+    int left = 0;
+    int right = n-1;
+
+    while(left<right){
+      int sum = nums[left] + nums[right];
+
+      if(sum == target){
+        return "Yes";
+      }
+      else if(sum<target)   left++;
+      else right--;
+    }
+    return "NO";
+}
 
 int main(){
 
-  int arr[9] = {1,1,0,1,1,1,0,1,1};
+  vector<int> nums = {2,7,11,15};
+  int target = 9;
 
   //* Brute force approach
 
-  // No brute force solution 
+  // vector<int> output = Brute(nums, target);
+
+  // for(auto& element:output ){
+  //   cout<<element;
+  // }
+  
+
+//* -------------------------------------------------------
+
+  //* Better approach
+
+  vector<int> output = Better(nums, target);
+
+  for(auto& element:output ){
+    cout<<element;
+  }
 
 //* -------------------------------------------------------
 
   //* Optimal approach
 
-  int maxi = 0;
-  int count = 0;
+  // cout<<Optimal(nums, target);
 
-  for(int i = 0; i<9; i++){
-    if(arr[i]==1){
-      count++;
-      maxi = max(maxi, count);
-    }
-    else{
-      count = 0;
-    }
-  }
-
-  cout<<maxi<<endl;
 }
