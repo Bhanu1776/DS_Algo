d64d5263f35cf94974188a2ac320744e656e7107
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-18 12:08:01 +0530

    Median of Two Sorted Arrays Optimal

--- revision.cpp
+++ revision.cpp
@@ -1,25 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Remove Element form an Array
+//? Median of Two Sorted Arrays
 
-int Optimal(vector<int>& nums, int val) {
-    int i = 0;           // Pointer to track the position of non-val elements
+//* Steps:-
+//  1) First create a new vector considering two given array sizes
+//  1) Merge both give arrays into a newly created one
+//  1) Sort the merged array
+//  1) Find the median
+//      a) Where size of the merged array is (even)
+//          - Find out using given formula (Hard formula bc!!)    //* Imp to remember
+//      b) Where (odd)
+//          - dived the size of merged array (simple formula)
 
-      for (int j = 0; j < nums.size(); j++) {
-          if (nums[j] != val) {
-              nums[i] = nums[j];
-              i++;
-          }
-      }
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> merged;
+    merged.reserve(nums1.size() + nums2.size());
+    
+    merged.insert(merged.end(), nums1.begin(), nums1.end());
+    merged.insert(merged.end(), nums2.begin(), nums2.end());
+    
+    sort(merged.begin(), merged.end());
 
-      return i;
+    int combinedLength = merged.size();
+    if (combinedLength % 2 == 0) {            // If the total number of elements in the merged array is even
+                                              // Calculate the median for an even number of elements
+        return (merged[combinedLength / 2 - 1] + merged[combinedLength / 2]) / 2.0;         //* Imp to remember
+    } else {                                  // If the total number of elements in the merged array is odd
+                                              // Calculate the median for an odd number of elements
+        return merged[combinedLength / 2];
+    }
 }
 
 int main(){
 
-  vector<int> nums = {1,1,1,2,2,3};
-  int valueToBeRemoved = 2;
+  vector<int> nums1 = {1,2};
+  vector<int> nums2 = {3,4};
 
   //* Brute force approach
 
@@ -29,7 +45,7 @@ int main(){
 
   //* Optimal approach
 
-  int output = Optimal(nums, valueToBeRemoved);
+  double output = findMedianSortedArrays(nums1, nums2);
   cout<<output;
 
 }
