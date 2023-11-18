c4066cbada5b277e486c23d8aaeb4b95f68a512e
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-18 11:11:23 +0530

    Remove Element form an Array Optimal

--- revision.cpp
+++ revision.cpp
@@ -1,60 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Remove Duplicates from Sorted Array II
+//? Remove Element form an Array
 
-int Brute(vector<int>& nums) {
-   map<int, int> countMap;                 // In the map, left will contain value and right will have count
+int Optimal(vector<int>& nums, int val) {
+    int i = 0;           // Pointer to track the position of non-val elements
 
-      for (int num : nums) {
-          countMap[num]++;            // Left side i'm storing value, if the same again repeats then it will increase the count by 1
-      }
-
-      int index = 0;
-      for (auto& pair : countMap) {
-          int num = pair.first;
-          int count = pair.second;
-
-          nums[index++] = num;            // First time i will store usually
-
-          if (count >= 2) {
-              nums[index++] = num;        // But second time i'll store if the count is greater than equal to 2!
+      for (int j = 0; j < nums.size(); j++) {
+          if (nums[j] != val) {
+              nums[i] = nums[j];
+              i++;
           }
       }
 
-      return index;
-}
-
-int Optimal(vector<int>& nums) {
-    if(nums.size()<=2){                 // base
-            return nums.size();
-        }
-
-        int i = 2;                    // Start from the third element
-        for(int j = 2; j<nums.size(); j++){         // Book me Dry run kar samhjega!!
-            if(nums[i-2] != nums[j]){
-                nums[i] = nums[j];
-                i++;
-            }
-        }
-        return i;
+      return i;
 }
 
 int main(){
 
   vector<int> nums = {1,1,1,2,2,3};
+  int valueToBeRemoved = 2;
 
   //* Brute force approach
 
-  int SizeOfArray = Brute(nums);
-  cout<<SizeOfArray;
-
+  // No brute force soln
 
 //* -------------------------------------------------------
 
   //* Optimal approach
 
-  int SizeOfArray = Optimal(nums);
-  cout<<SizeOfArray;
+  int output = Optimal(nums, valueToBeRemoved);
+  cout<<output;
 
 }
