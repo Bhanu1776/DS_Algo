42f37a202afc589625daf5684f7f3335d699e4f8
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-19 12:27:36 +0530

    Set Matrix Zeros Better Approach

--- revision.cpp
+++ revision.cpp
@@ -1,42 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Container With Most Water
+//? Set Matrix Zeros
 
-int maxArea(vector<int>& height) {
-    int n = height.size();
-    int left = 0;         // At start
-    int right = n-1;      // At end
-    int ans = 0;          // Storing the max area
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
 
-    while(left<right){
-        ans = max(ans, (right-left)*min(height[left], height[right]));        // Dry run this based on given histograms
-                                          // We want max area so that we can understand between which histograms max water we are able to store
-        if(height[left]<height[right]){
-            left++;
-        }
-        else{
-            right--;
-        }
-    }
-    return ans;
+	int baharKaRow[n] = {0};
+	int baharKaCol[m] = {0};
+
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<m; j++){
+			if(matrix[i][j]==0){
+				baharKaRow[i] = 1;
+				baharKaCol[j] = 1;
+			}
+		}
+	}
+
+	for(int i = 0; i<n; i++){
+		for(int j= 0; j<m; j++){
+			if(baharKaRow[i] ==1 || baharKaCol[j] == 1){
+				matrix[i][j]=0;
+			}
+		}
+	}
+	return matrix;
 }
 
 int main(){
 
-  vector<int> height = {1,8,6,2,5,4,8,3,7};
+  vector<vector<int>> matrix = {{1,1,1},{1,0,1},{1,1,1}};
+  int n = matrix.size();
+  int m = matrix[0].size();
 
+  vector<vector<int>> output = zeroMatrix(matrix, n, m);
+  
+  for(int i = 0; i<n; i++){
+		for(int j= 0; j<m; j++){
+			cout<<matrix[i][j]<<' ';
+		}
+    cout<<endl;
+	}
   //* Brute force approach
 
-  // No brute force soln
+  // We will create exact same array with all the set zeros
+  // This will take extra space and time.
 
 //* -------------------------------------------------------
 
-  //* Optimal approach
+  //* Better approach (Best)
 
-  int output = maxArea(height);
-  cout<<output;
+  
+
+
+//* -------------------------------------------------------
+
+  //* Optimal approach (Hard)
+
+  // In optimal approach we are not going to use any dummy array this will reduce space
+  // Instead of we will consider matrix's first row and col as a dummy array
+  // But there's a catch the very first element of matrix is going to present for both col and row
+  // So for that first element we will store the value in another variable in the form of boolean 
 
 }
-
-//* Leetcode problem link: https://leetcode.com/problems/container-with-most-water/
