85a5abb99ab6308dea213b3a20a6d6589887b971
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-20 12:49:12 +0530

    Rotate Image by 90deg Optimal

--- revision.cpp
+++ revision.cpp
@@ -1,36 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Best Time to Buy and Sell Stock
+//? Rotate Image by 90deg
 
-int maxProfit(vector<int>& prices) {
+// The first row will be the last col
+// The second row will be the second last col
+// The third row will be the third last col
 
-    int mini = INT_MAX;
-    int profit = 0;
+//*  1 2 3         7 4 1
+//*  4 5 6   ==>   8 5 2
+//*  7 8 9         9 6 3
 
-    for(int i = 0; i<prices.size(); i++){
-        mini = min(mini, prices[i]);
-        profit = max(profit, prices[i] - mini);
+
+void rotate(vector<vector<int>>& matrix) {      
+    int n = matrix.size();
+
+    for(int i = 0; i<n; i++){                  //* Step 1) Finding out the transpose
+        for(int j=0; j<i; j++){
+            swap(matrix[i][j], matrix[j][i]);   // Remember this way!
+        }
     }
 
-    return profit;
-
+    for(int i = 0; i<n; i++){
+        reverse(matrix[i].begin(), matrix[i].end());    //* Step 2) Reversing the transposed matrix
+    }
 }
+
 
 int main(){
 
-  vector<int> prices = {7,1,5,3,6,4};
+  vector<vector<int>> matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
 
   //* Brute force approach
 
-  // Running two loops comparing with each and every number and getting the final output.
+  // We will be placing the first row into new matrix as last col, second row as last second col, likewise...
   
 
 //* -------------------------------------------------------
 
   //* Optimal approach 
 
-  int profit = maxProfit(prices);
-  cout<<profit;
-    
+//? Observation from the above matrix:
+// Diagonal elements will always be same when rotated
+// When we transpose the og matrix and later reverse the whole matrix will give the resultant output
+
+  rotate(matrix);
+
+  for(int i = 0; i<matrix.size(); i++){                  
+    for(int j=0; j<matrix[0].size(); j++){
+        cout<<matrix[i][j]<<' ';
+    }
+    cout<<endl;
+  }
 }
