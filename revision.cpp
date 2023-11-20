15ac54759c00520db4e764c3cc911f584f28f91c
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-20 10:52:33 +0530

    Maximum Subarray - Kadane's algorithm Optimal Only!

--- revision.cpp
+++ revision.cpp
@@ -1,74 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Pascal's Triangle
+//? Maximum Subarray - Kadane's algorithm - Easy AF!
 
-//* Approach One
+int maxSubArray(vector<int>& nums) {
+    int curr_sum = 0;
+    int max_sum = INT_MIN;
 
-// This function will take row number as input and returns the entire row values on that row index
-vector<int> generateRow(int row){
-    long long ans = 1;                      // The row will generally starts wtih 1 (for every pascals row )
-    vector<int> ansRow;
-    ansRow.push_back(1);
+    for(int i = 0; i<nums.size(); i++){         //* Dry run karle
+        curr_sum = curr_sum + nums[i];
+        max_sum = max(max_sum, curr_sum);
 
-    for(int col = 1; col<row; col++){
-        ans = ans * (row - col);
-        ans = ans/col;
-        ansRow.push_back(ans);
+        if(curr_sum<0)  curr_sum = 0;
     }
-    return ansRow;
-}
 
-vector<vector<int>> Hard(int N) {
-    
-    vector<vector<int>> ans;
-    for(int i = 1; i<=N; i++){
-        ans.push_back(generateRow(i));
-    }
-    return ans;
-}
-
-//* -------------------------------------------------------
-
-
-//* Approach Two  -- Refer this!!
-vector<vector<int>> Easy(int numRows) {
-        
-    vector<vector<int>> ans(numRows);           // This to return final ans
-
-    for(int i = 0; i<numRows; i++){
-        vector<int> temp(i+1, 1);               // Creating new 1d vector so that we can store the calculated values
-                                                // Initially this vector is going to be filled with all ones
-
-        for(int j = 1; j<i; j++){               // Traverse the elements from the second to second-last position of the current row
-            temp[j] = ans[i-1][j] + ans[i-1][j-1];      // Val ke uppar wale row ka value and uske baju wale value ka addition
-        }
-        ans[i] = temp;                          // Store the current row in the Pascal's Triangle
-    }
-    return ans;
+    return max_sum;
 }
 
 int main(){
 
-  int numRows = 5;
+  vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
 
+  //* Brute force approach
 
-  //* Better approach (Best)
-
-  // vector<vector<int>> output = Hard(numRows);
+  // In Brute force approach total 3 loops will run and will find the maximum
   
 
 //* -------------------------------------------------------
 
-  //* Optimal approach (Hard)
+  //* Optimal approach - Using Kadane's Algorithm
 
-  vector<vector<int>> output = Easy(numRows);
-  
-  for(int i = 0; i<output.size(); i++){
-		for(int j= 0; j<i+1; j++){
-			cout<<output[i][j]<<' ';
-		}
-    cout<<endl;
-	}
-
+    int maxSum = maxSubArray(nums);
+    cout<<maxSum<<endl;
 }
