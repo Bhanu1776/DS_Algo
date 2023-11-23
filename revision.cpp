9b740b2f61231190a4818eedd739a8a789b368e4
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-23 11:06:53 +0530

    Missing and repeating numbers Using mathematical equations

--- revision.cpp
+++ revision.cpp
@@ -1,42 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Find the Duplicate Number   - Using Floyd's Cycle Detection algo
-// In an array only one number will be duplicate number
+//? Missing and repeating numbers
 
-int findDuplicate(vector<int>& nums) {
+pair<int, int> missingAndRepeating(vector<int> &A, int n) {
 
-    int slow = nums[0];
-    int fast = nums[0];
+  int x = 0, y = 0;
 
-    do{
-        slow = nums[slow];                    // Initially slow pointer will move only one step ahead
-        fast = nums[nums[fast]];              // Fast will move two steps
-    }while(slow!=fast);
+  long long sum = (n * (n + 1)) / 2;            // Calculate the sum of the first n natural numbers
+  long long sum2 = (n * (n + 1) * ((2 * n) + 1)) / 6;       // Calculate the sum of squares of the first n natural numbers
 
-    slow = nums[0];                           //* Whenever slow and fast pointer meets, we will place slow to the very first position
-    while(slow!=fast){
-        slow = nums[slow];
-        fast = nums[fast];                    // And Both slow and fast pointer will only move one step
-    }
-    return slow;                              // Wherever both meets again, that number will be the duplicate one!
+  for (auto i : A) {                            //* Dry run kar!
+    sum -= 1ll * i;                             // Subtract each element from the total sum (sum will represent x - y)       
+    sum2 -= (1ll * i * i);                      // Subtract the square of each element from the sum of squares (sum2 will represent x^2 - y^2)
+  }                                             //* ll after one stands for long long
+
+  x = (sum2 - (sum * sum)) / (2 * sum);         // Finding repeating Number
+  y = (sum + x);                                // Finding Missing number
+
+  return {y, x};
 }
+
 
 int main(){
 
-  vector<int> nums = {3,1,3,4,2};
+  vector<int> nums = {3,3,2,1};
+  int n = nums.size();
 
 
   //* Brute force approach
-  // In the brute force approach, we will first sort the array
-  // Then we will run the loop and check any same number placed twice, that will be the answer
+
+  // Creating 1 to n numbers array
+  // Comparing it with given array, we will mark frequencies
+  // If one element is present freq would be one! and if repeating numbers are found freq would be 2
+  // In the same way if freq is 0 then it would be missing number
 
 
 //* -------------------------------------------------------
 
   //* Optimal approach 
 
-  int output = findDuplicate(nums);
-  cout<<output<<endl;
+  pair<int, int> myPair = missingAndRepeating(nums, n);
+ 
+    cout << myPair.first << " ";
+    cout << myPair.second << endl;
+
 
 }
