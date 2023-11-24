2477564879061c743475364822f3f39c1af1e05d
Bhanu1776 <sunkabhanu281202@gmail.com>
2023-11-24 14:39:23 +0530

    Reverse Pairs using Merge sort

--- revision.cpp
+++ revision.cpp
@@ -1,43 +1,79 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//? Grid Unique Paths
-// In a grid, we can only move down and right side
-
-//* Brute force soln        - Using recursion - T.c will be exponential ==> O(2^(n+m))
-int countPaths(int i, int j, int n, int m){     //* For better understanding draw recursive tree
-    if(i==(n-1) && j==(m-1))    return 1;       // Whenever we reach at the end of every branch node, that time return 1;
-    if(i>=n || j>=m)    return 0;               // This is when we go out of boundary, return 0;
-    else return countPaths(i+1, j, n, m) + countPaths(i, j+1, n, m);         // Traversing through the recursive tree
-}
+//? Reverse Pairs
+// a[i] > 2 * a[j]
+// [4, 6, 2] ==> In this given array 6 is greater than 2*2=4, so this pair will be count as 1, likewise we need to find all the pairs
 
 
-//* Optimal soln
-int uniquePaths(int m, int n) {                 // Dry run the code to understand better!
-	int N = n+m - 2;                            // This is to move from the top-left corner to the bottom-right corner in a grid.
-	int r = m - 1;                              // In short we are finding NCr
-	double result = 1;
+//* Typical merge sort code and adding our logic in between
+void merge(vector<int> &nums, int l, int m, int r){
+      int count = 0;
 
-	for(int i = 1; i<=r; i++){
-		result = result * (N-r+i) / i;          // (N-r+i) is the numerator and /i is denominator
-	}
-	return (int)result;
-}
+      //copy into right & left array
+      vector<int>left(nums.begin()+l,nums.begin()+m+1);
+      vector<int>right(nums.begin()+m+1,nums.begin()+r+1);
+
+      int i = 0, j = 0, n1 = left.size(),n2 = right.size();
+      
+      while(i < n1 && j < n2){
+          if (left[i] <= right[j])
+              nums[l++] = left[i++];
+          else{
+            nums[l++] = right[j++];
+          }     
+      }
+      while (i < n1)
+          nums[l++] = left[i++];
+      while (j < n2)
+          nums[l++] = right[j++];
+  }
+
+
+  //* This is the logic
+  int countPairs(vector<int>&nums,int l, int m, int r){
+      int count = 0;
+      for(int i = l, j = m+1; i <= m; i++){
+          while(j <= r && nums[i] > 2LL*nums[j]) j++;
+          count += j-(m+1);
+      }
+      return count;
+  }
+
+
+  int mergeSort(vector<int> &nums, int l, int r){
+      int count = 0;
+      if (l == r) return count;
+      
+      int m = l + (r-l)/2;
+      
+      count += mergeSort(nums,l,m);             //left part sort
+      count += mergeSort(nums,m+1,r);           //right part sort
+      count += countPairs(nums,l,m,r);          //count pairs
+      merge(nums,l,m,r);                        //merge these sorted array
+
+      return count;
+  }
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums,0,nums.size()-1);
+    }
 
 int main(){
 
-  int m = 2, n = 3;
+  vector<int> nums = {1,3,2,3,1};
 
   //* Brute force approach
 
-  int output = countPaths(0, 0, 3, 7);
-  cout<<output<<endl;
+  // In the brute force approach, we will go from i=0 to n-1 and then another loop that will start from j=i+1 to n-1
+  // wherever we find a[i] > 2 * a[j] we will increase the count;
+  // Time complexity will be O(n^2) and S.C will be O(1)
+
  
 //* -------------------------------------------------------
 
-  //* Optimal approach 
+  //* Optimal approach -- Its pretty difficult
 
-  int totalCount= uniquePaths(2,3);
-    cout<<totalCount;
+  int output = reversePairs(nums);
+  cout<<output;
 
 }
